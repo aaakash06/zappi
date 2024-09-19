67dffd44f55469e107a8e63b5b6ad64750481b00
aaakash06 <akashbagale182@gmail.com>
2024-09-19 15:25:52 +0545

    producer done

---
+++ apps/producer/src/index.ts
@@ -0,0 +1,41 @@
+import { Kafka } from "kafkajs";
+import client from "@repo/db/client";
+
+const TOPIC_NAME = "zap-events";
+
+const kafka = new Kafka({
+  clientId: "outbox-processor",
+  brokers: ["localhost:9092"],
+});
+
+const db = client;
+
+const main = async () => {
+  const producer = kafka.producer();
+  await producer.connect();
+  while (1) {
+    const pendingRows = await db.zapRunOutbox.findMany({
+      where: {},
+      take: 10,
+    });
+    // Producing
+    await producer.send({
+      topic: TOPIC_NAME,
+      messages: pendingRows.map((row) => {
+        return {
+          value: row.zapRunId,
+        };
+      }),
+    });
+    await client.zapRunOutbox.deleteMany({
+      where: {
+        id: {
+          in: pendingRows.map((r) => r.id),
+        },
+      },
+    });
+    await new Promise((r) => setTimeout(r, 3000));
+  }
+};
+
+main().catch(console.error);
