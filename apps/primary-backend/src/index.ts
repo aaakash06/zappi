83487499a44b524d55bf7521491c87193a04fdb6
aaakash06 <akashbagale182@gmail.com>
2024-09-19 19:39:47 +0545

    failed tsup try

--- apps/primary-backend/src/index.ts
+++ apps/primary-backend/src/index.ts
@@ -1,16 +1,21 @@
-import express, { Router } from "express";
-import { userRouter } from "./routers/user";
-import { zapRouter } from "./routers/zap";
-import cors from "cors";
+// import express, { Router } from "express";
+// import { userRouter } from "./routers/user";
+// import { zapRouter } from "./routers/zap";
+// import cors from "cors";
+// import { db } from "./main";
+import client from "@repo/db/client";
 
-const app = express();
+// const app = express();
 
-app.use(express.json());
-app.use(cors());
+// app.use(express.json());
+// app.use(cors());
 
-app.use("/user", userRouter);
-app.use("/zap", zapRouter);
+// app.use("/user", userRouter);
+// app.use("/zap", zapRouter);
+// console.log(db);
 
-app.listen(3000, () => {
-  console.log("server is listening");
-});
+// app.listen(3000, () => {
+//   console.log("server is listening");
+// });
+
+console.log(client);
